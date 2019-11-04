e9c4582928106ca46030f670d58e08755142284d
Alex Forrester <alexforrester96@gmail.com>
2019-11-04 15:52:44 +0000

    added server side kinda

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,16 +4,19 @@
     <h1>ISS Tracker</h1>
 
     <iss-map></iss-map>
+    <quiz></quiz>
   </div>
 </template>
 
 <script>
 import issMap from "./components/issMap.vue";
+import quiz from "./components/quiz.vue"
 
 export default {
   name: 'app',
   components: {
-    'iss-map': issMap
+    'iss-map': issMap,
+    'quiz': quiz
   },
   data(){
     return {
---
+++ client/src/components/quiz.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: "quiz"
+}
+</script>
+
+<style lang="css" scoped>
+</style>
