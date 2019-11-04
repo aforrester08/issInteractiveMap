2134686ffbcdc1f12d3377c3a4123b3f6d014378
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-11-04 15:58:08 +0000

    chart created

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,16 +4,19 @@
     <h1>ISS Tracker</h1>
 
     <iss-map></iss-map>
+    <chart></chart>
   </div>
 </template>
 
 <script>
 import issMap from "./components/issMap.vue";
+import chart from "./components/chart.vue";
 
 export default {
   name: 'app',
   components: {
-    'iss-map': issMap
+    'iss-map': issMap,
+    'chart': chart
   },
   data(){
     return {
---
+++ client/src/components/chart.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <GChart
+  type="ColumnChart"
+  :data="chartData"
+  :options="chartOptions"
+  />
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [
+        ['Vehicle', 'Speed km/h'],
+        ['ISS', 27576],
+        ['Aeroplane', 928.8],
+        ['Train', 349.2],
+        ['Car', 486]
+      ],
+      chartOptions: {
+        chart: {
+          title: 'Speed by Vehicle'
+
+        }
+      }
+    }
+  }
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
