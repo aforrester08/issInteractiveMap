d2928bfaecd019ceec7fc110618a6c4a83dd0537
Alex Forrester <alexforrester96@gmail.com>
2019-11-05 09:47:35 +0000

    server

---
+++ server/db/seeds.js
@@ -0,0 +1,25 @@
+
+
+[
+
+  {
+    "question": "How much did NASA contribute towards the ISS?",
+    "answers": {
+      "option1": {"$58.7 Billion", "correct": true},
+      "option2": {"$45 Billion", "correct": false},
+      "option3": {"$12.3 Billion", "correct": false},
+      "option4": {"$5 Billion", "correct": false},
+    }
+  },
+
+  {
+    "question": "How many minutes does it take for the ISS to complete one orbit?",
+    "answers": {
+      "option1": {"37", "correct": false},
+      "option2": {"117", "correct": false},
+      "option3": {"92", "correct": true},
+      "option4": {"29", "correct": false},
+    }
+  }
+
+]
