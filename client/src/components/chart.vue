d172b08d4d6b3304297d0ae7c9f7510aa9f4bb2d
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-11-05 09:45:57 +0000

    change chart data

--- client/src/components/chart.vue
+++ client/src/components/chart.vue
@@ -1,9 +1,18 @@
 <template lang="html">
-  <GChart
-  type="ColumnChart"
-  :data="chartData"
-  :options="chartOptions"
-  />
+  <div class="">
+    <h2>Space walks</h2>
+    <ul>
+      <li>There have been 221 space walks at the International Space Station.</li>
+      <li>There have been 167 space walks in US spacesuits.</li>
+      <li>There have been 54 space walks in  Russian spacesuits.</li>
+    </ul>
+
+    <GChart
+    type="ColumnChart"
+    :data="chartData"
+    :options="chartOptions"
+    />
+  </div>
 </template>
 
 <script>
@@ -12,13 +21,30 @@ export default {
 
   data () {
     return {
-      // Array will be automatically processed with visualization.arrayToDataTable function
       chartData: [
-        ['Vehicle', 'Speed km/h'],
-        ['ISS', 27576],
-        ['Aeroplane', 928.8],
-        ['Train', 349.2],
-        ['Car', 486]
+        ['Year', 'Number of Space walks'],
+        ['1998', 3],
+        ['1999', 1],
+        ['2000', 9],
+        ['2001', 18],
+        ['2002', 18],
+        ['2003', 2],
+        ['2004', 5],
+        ['2005', 7],
+        ['2006', 14],
+        ['2007', 23],
+        ['2008', 19],
+        ['2009', 17],
+        ['2010', 15],
+        ['2011', 10],
+        ['2012', 5],
+        ['2013', 11],
+        ['2014', 7],
+        ['2015', 7],
+        ['2016', 4],
+        ['2017', 10],
+        ['2018', 8],
+        ['2019', 8]
       ],
       chartOptions: {
         chart: {
